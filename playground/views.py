d0663f21ba28acb493f599a1fdd907f0430dec66
Xabier Larrakoetxea <slok69@gmail.com>
2012-09-10 20:36:03 +0200

    Declare the view methods for the different urls

--- playground/views.py
+++ playground/views.py
@@ -5,3 +5,15 @@ from django.template import RequestContext
 def index(request):
     return render_to_response('playground/index.html',
         context_instance=RequestContext(request))
+
+def echo(request):
+    return render_to_response('playground/echo.html',
+        context_instance=RequestContext(request))
+
+def time(request):
+    return render_to_response('playground/time.html',
+        context_instance=RequestContext(request))
+
+def chat(request):
+    return render_to_response('playground/chat.html',
+        context_instance=RequestContext(request))
