ae37618ad2dd1885b173e319da36fb875cf867f4
Xabier Larrakoetxea <slok69@gmail.com>
2012-09-10 20:33:38 +0200

    Added basic socketio implementation structure

---
+++ playground/socketioplayground.py
@@ -0,0 +1,39 @@
+import datetime
+
+from socketio.namespace import BaseNamespace
+from socketio.mixins import RoomsMixin, BroadcastMixin
+from socketio.sdjango import namespace
+
+
+@namespace('/echo')
+class EchoNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+
+    def initialize(self):
+        print("Socketio echo session started")
+
+    def on_echo(self, msg):
+        print('Echo message')
+        self.broadcast_event('echo msg', msg)
+        return True
+
+
+@namespace('/time')
+class EchoNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+
+    def initialize(self):
+        print("Socketio time session started")
+
+    def on_update_time(self, garbage):
+        print('Update time')
+        self.broadcast_event('update client time', str(datetime.datetime.now()))
+        return True
+
+
+@namespace('/chat')
+class EchoNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+
+    def initialize(self):
+        print("Socketio chat session started")
+
+    def on_say(self, msg):
+        return True
