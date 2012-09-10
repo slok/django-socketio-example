0363670bc86f04c37daad73a09ddae77681f6a17
Xabier Larrakoetxea <slok69@gmail.com>
2012-09-10 20:21:41 +0200

    new urls for the socketio examples

--- playground/urls.py
+++ playground/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'playground.views.index'),
+    url(r'^/echo$', 'playground.views.echo'),
+    url(r'^/time$', 'playground.views.time'),
+    url(r'^/chat$', 'playground.views.chat'),
 )
