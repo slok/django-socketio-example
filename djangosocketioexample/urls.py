f84e21926cb28af8f009ee68e123ac4798fa24da
Xabier Larrakoetxea <slok69@gmail.com>
2012-09-10 18:58:09 +0200

    Added socketio urls

--- djangosocketioexample/urls.py
+++ djangosocketioexample/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
+import socketio
+
 
 urlpatterns = patterns('',
     url(r'^$', 'djangosocketioexample.views.index'),
     url(r'^playground/', include('playground.urls')),
+    url(r'^socket\.io', include(socketio.sdjango.urls))
 )
