eb0e03d4ef7dade1bc1917597f9bb53360ff6eba
Xabier Larrakoetxea <slok69@gmail.com>
2012-09-10 20:38:29 +0200

    Fixe playground urls

--- djangosocketioexample/urls.py
+++ djangosocketioexample/urls.py
@@ -6,7 +6,7 @@ import socketio.sdjango
 
 urlpatterns = patterns('',
     url(r'^$', 'djangosocketioexample.views.index'),
-    url(r'^playground/', include('playground.urls')),
+    url(r'^playground', include('playground.urls')),
     url(r'^socket\.io', include(socketio.sdjango.urls))
 )
 
--- playground/urls.py
+++ playground/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'playground.views.index'),
-    url(r'^/echo$', 'playground.views.echo'),
-    url(r'^/time$', 'playground.views.time'),
-    url(r'^/chat$', 'playground.views.chat'),
+    url(r'/$', 'playground.views.index'),
+    url(r'/echo$', 'playground.views.echo'),
+    url(r'/time$', 'playground.views.time'),
+    url(r'/chat$', 'playground.views.chat'),
 )
